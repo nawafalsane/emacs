96589632804ec1f9ea78c588a32f9f378825fbe9
nawafalsane <nawafalsane@gmail.com>
2020-12-30 15:00:25 +0300

    Emacs directory

---
+++ init.el
@@ -0,0 +1,151 @@
+;; Set up Package and Download use-package
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/")
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+(defconst private-dir  (expand-file-name "private" user-emacs-directory))
+(defconst temp-dir (format "%s/cache" private-dir)
+  "Hostname-based elisp temp directories")
+;; Core settings
+;; UTF-8 please
+(set-charset-priority 'unicode)
+(setq locale-coding-system   'utf-8)   ; pretty
+(set-terminal-coding-system  'utf-8)   ; pretty
+(set-keyboard-coding-system  'utf-8)   ; pretty
+(set-selection-coding-system 'utf-8)   ; please
+(prefer-coding-system        'utf-8)   ; with sugar on top
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+
+;; Emacs customizations
+(setq confirm-kill-emacs                  'y-or-n-p
+      confirm-nonexistent-file-or-buffer  t
+      save-interprogram-paste-before-kill t
+      mouse-yank-at-point                 t
+      require-final-newline               t
+      visible-bell                        nil
+      ring-bell-function                  'ignore
+      ;; http://ergoemacs.org/emacs/emacs_stop_cursor_enter_prompt.html
+      minibuffer-prompt-properties
+      '(read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)
+
+      ;; Disable non selected window highlight
+      cursor-in-non-selected-windows     nil
+      highlight-nonselected-windows      nil
+      ;; PATH
+      exec-path                          (append exec-path '("/usr/local/bin/"))
+      indent-tabs-mode                   nil
+      inhibit-startup-message            t
+      fringes-outside-margins            t
+      x-select-enable-clipboard          t
+      use-package-always-ensure          t)
+
+;; Bookmarks
+(setq
+ ;; persistent bookmarks
+ bookmark-save-flag                      t
+ bookmark-default-file              (concat temp-dir "/bookmarks"))
+
+;; Backups enabled, use nil to disable
+(setq
+ history-length                     1000
+ backup-inhibited                   nil
+ make-backup-files                  t
+ auto-save-default                  t
+ auto-save-list-file-name           (concat temp-dir "/autosave")
+ create-lockfiles                   nil
+ backup-directory-alist            `((".*" . ,(concat temp-dir "/backup/")))
+ auto-save-file-name-transforms    `((".*" ,(concat temp-dir "/auto-save-list/") t)))
+
+(unless (file-exists-p (concat temp-dir "/auto-save-list"))
+		       (make-directory (concat temp-dir "/auto-save-list") :parents))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(global-auto-revert-mode t)
+
+;; Disable toolbar & menubar
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (  fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(show-paren-mode 1)
+
+
+;; Delete trailing whitespace before save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Theme for emacs
+(use-package dracula-theme
+  :defer t
+  :init
+  (load-theme 'dracula t)
+  :ensure t )
+
+
+;; which-key
+(use-package which-key
+  :ensure t)
+
+;; Evil mode
+(use-package evil
+  :ensure t ;; install the evil package if not installed
+  :init ;; tweak evil's configuration before loading it
+  (setq evil-search-module 'evil-search)
+  (setq evil-ex-complete-emacs-commands nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (setq evil-shift-round nil)
+  (setq evil-want-C-u-scroll t)
+  ;; Change Cursor Color
+  (setq evil-emacs-state-cursor '("red" box))
+  (setq evil-normal-state-cursor '("pink" box))
+(setq evil-visual-state-cursor '("orange" box))
+(setq evil-insert-state-cursor '("pink" bar))
+(setq evil-replace-state-cursor '("pink" bar))
+(setq evil-operator-state-cursor '("pink" hollow))
+ :config ;; tweak evil after loading it
+  (evil-mode)
+  )
+(use-package powerline
+  :ensure t
+  )
+
+(use-package airline-themes
+  :init
+  (progn
+    (require 'airline-themes)
+(load-theme 'airline-dark_minimal t)
+    )
+  :config
+  (progn
+    (setq powerline-utf-8-separator-left        #xe0b0
+          powerline-utf-8-separator-right       #xe0b2
+          airline-utf-glyph-separator-left      #xe0b0
+          airline-utf-glyph-separator-right     #xe0b2
+          airline-utf-glyph-subseparator-left   #xe0b1
+          airline-utf-glyph-subseparator-right  #xe0b3
+          airline-utf-glyph-branch              #xe0a0
+          airline-utf-glyph-readonly            #xe0a2
+          airline-utf-glyph-linenumber          #xe0a1)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
